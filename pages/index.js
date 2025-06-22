32dde9e54cdb046b4cc331fc4b40a423fd4757ff
fcpdlr <fernando.pdelarosa@gmail.com>
2025-06-22 10:58:59 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ export default function Home() {
     const { data, error } = await supabase
       .from('entity_rankings')
       .select(
-        'id, elo_rating, entity_id, entities (name, name_line1, name_line2, name_line3, image_url)'
+        'id, elo_rating, id, entities (name, name_line1, name_line2, name_line3, image_url)'
       )
       .eq('entity_category_id', ENTITY_CATEGORY_ID)
       .order('elo_rating', { ascending: false })
@@ -219,11 +219,11 @@ const { error: voteError } = await supabase.rpc('vote_and_update_elo', {
       {duel.length === 2 && (
         <section className="flex flex-col items-center justify-center py-4">
           <div className="relative flex flex-row items-center justify-center gap-6 h-40">
-            <button onClick={() => vote(duel[0].entity_id, duel[1].entity_id)} className="w-40 h-40 rounded-xl overflow-hidden border transition hover:brightness-110 focus:outline-none relative">
+            <button onClick={() => vote(duel[0].id, duel[1].id)} className="w-40 h-40 rounded-xl overflow-hidden border transition hover:brightness-110 focus:outline-none relative">
               <img
                 src={duel[0].image_url}
                 alt={duel[0].name_line2 || duel[0].name_line1}
-                className={`w-full h-full object-cover transition duration-300 ease-in-out ${selected === duel[0].entity_id ? 'scale-110 ring-4 ring-goat z-10 shadow-[0_0_20px_rgba(255,165,0,0.8)]' : ''}`}
+                className={`w-full h-full object-cover transition duration-300 ease-in-out ${selected === duel[0].id ? 'scale-110 ring-4 ring-goat z-10 shadow-[0_0_20px_rgba(255,165,0,0.8)]' : ''}`}
               />
             </button>
 
@@ -233,11 +233,11 @@ const { error: voteError } = await supabase.rpc('vote_and_update_elo', {
               </div>
             </div>
 
-            <button onClick={() => vote(duel[1].entity_id, duel[0].entity_id)} className="w-40 h-40 rounded-xl overflow-hidden border transition hover:brightness-110 focus:outline-none relative">
+            <button onClick={() => vote(duel[1].id, duel[0].id)} className="w-40 h-40 rounded-xl overflow-hidden border transition hover:brightness-110 focus:outline-none relative">
               <img
                 src={duel[1].image_url}
                 alt={duel[1].name_line2 || duel[1].name_line1}
-                className={`w-full h-full object-cover transition duration-300 ease-in-out ${selected === duel[1].entity_id ? 'scale-110 ring-4 ring-goat z-10 shadow-[0_0_20px_rgba(255,165,0,0.8)]' : ''}`}
+                className={`w-full h-full object-cover transition duration-300 ease-in-out ${selected === duel[1].id ? 'scale-110 ring-4 ring-goat z-10 shadow-[0_0_20px_rgba(255,165,0,0.8)]' : ''}`}
               />
             </button>
           </div>
