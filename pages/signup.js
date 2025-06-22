768a699695c297008001644d5ee81a0cd8b5c01c
fcpdlr <fernando.pdelarosa@gmail.com>
2025-06-22 11:25:05 +0200

    Update signup.js

--- pages/signup.js
+++ pages/signup.js
@@ -8,8 +8,15 @@ countries.registerLocale(enLocale)
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  {
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+    },
+  }
 )
+
 
 const countryList = Object.entries(
   countries.getNames('en', { select: 'official' })
