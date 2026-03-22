74c7f0b519cc4b6b7ba1d95eda447e4ac0fe4d81
fcpdlr <fernando.pdelarosa@gmail.com>
2026-03-22 18:03:55 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -270,7 +270,7 @@ export default function Home() {
                     className="w-full h-full object-cover"
                   />
                   {/* gradiente oscuro sobre la imagen */}
-                  <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/20 to-transparent" />
+                  <div className="absolute inset-0 bg-gradient-to-t from-black/50 via-black/20 to-transparent" />
                   {/* nombre encima de la foto */}
                   <div className="absolute bottom-0 left-0 right-0 p-3 text-left">
                     {player.name_line1 && (
